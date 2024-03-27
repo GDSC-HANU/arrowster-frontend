3ce841054431c19dd6cfeb11fc7f658ad08beba1
ng-namanh <namanh200313@gmail.com>
2024-03-28 03:00:26 +0700

    chore: config input and inputMenu theme

--- src/app.config.ts
+++ src/app.config.ts
@@ -4,10 +4,26 @@ export default defineAppConfig({
     button: {
       color: {
         blue: {
-          solid: 'bg-blue-700 text-white'
+          solid: 'bg-blue-700 text-white hover:bg-blue-800'
         },
         white: {
           outline: 'border border-gray-400 ring-gray-500'
+        }
+      }
+    },
+    input: {
+      color: {
+        white: {
+          outline:
+            'ring-gray-500 bg-gray-50 border-gray-300 border border-gray-300 bg-gray-50! text-black'
+        }
+      }
+    },
+    inputMenu: {
+      color: {
+        white: {
+          outline:
+            'ring-gray-500 bg-gray-50 border-gray-300 border border-gray-300 bg-gray-50! text-black'
         }
       }
     }
