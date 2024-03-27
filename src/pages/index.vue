538d0794abe2f956b00ec4d05a632d402df582c4
vietqtran <tranquocviet1303@gmail.com>
2024-03-27 17:24:34 +0700

    change to webp format

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
       <h1 class="text-red-500">{{ counterStore.count }}</h1>
       <UButton @click="counterStore.increment">Increment</UButton>
     </div>
-    <NuxtImg width="200" src="/images/logo/Logo.svg" />
+    <NuxtImg format="webp" width="200" src="/images/logo/Logo.svg" />
     <nuxt-link to="/login">login</nuxt-link>
   </NuxtLayout>
 </template>
