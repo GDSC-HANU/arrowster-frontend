c42ece4d19cde0c43b597427e4a27e8327a8c6ec
ng-namanh <namanh200313@gmail.com>
2024-03-30 15:15:19 +0700

    chore: add supabase & mdi icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'light'
   },
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/device',
     '@pinia/nuxt',
@@ -17,12 +17,30 @@ export default defineNuxtConfig({
           'DM Sans': [400, 700]
         }
       }
-    ]
+    ],
+    '@nuxtjs/supabase'
   ],
   srcDir: 'src',
   pinia: {
     storesDirs: ['./stores/**']
   },
   css: ['~/assets/css/global.css', '~/assets/css/variables.css'],
-  plugins: ['~/plugins/theme.ts']
+  plugins: ['~/plugins/theme.ts'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+    redirectOptions: {
+      login: '/',
+      callback: '/confirm'
+    }
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+    }
+  },
+  ui: {
+    icons: ['mdi']
+  }
 })
