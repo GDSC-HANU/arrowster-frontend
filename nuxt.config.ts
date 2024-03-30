eaae232f86657753a8a6bdd1d175a08b928807ab
ng-namanh <namanh200313@gmail.com>
2024-03-30 12:08:42 +0700

    chore: add nuxt device

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   modules: [
+    '@nuxtjs/device',
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
     '@nuxt/ui',
